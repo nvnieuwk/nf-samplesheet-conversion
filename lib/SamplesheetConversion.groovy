6237e37cc4b65b781b40c7f442274556659eb866
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2023-01-09 14:18:35 +0100

    better parsing if a non-required field is missing

--- lib/SamplesheetConversion.groovy
+++ lib/SamplesheetConversion.groovy
@@ -38,9 +38,9 @@ class SamplesheetConversion {
                 throw new Exception("[Samplesheet Error] The samplesheet contains following unwanted field(s): ${samplesheetDifferences}")
             }
 
-            def ArrayList schemaDifferences = allFields.intersect(differences)
-            if(schemaDifferences.size > 0) {
-                throw new Exception("[Samplesheet Error] The samplesheet must contain '${allFields.join(",")}' as header field(s), but is missing these: ${schemaDifferences}")
+            def ArrayList requiredDifferences = requiredFields.intersect(differences)
+            if(requiredDifferences.size > 0) {
+                throw new Exception("[Samplesheet Error] The samplesheet requires '${requiredFields.join(",")}' as header field(s), but is missing these: ${requiredDifferences}")
             }
 
             samplesheet = Channel.value(samplesheetFile).splitCsv(header:true, strip:true, sep:delimiter)
@@ -62,34 +62,27 @@ class SamplesheetConversion {
                 
                 def String input = row[key]
 
-                if(input == null && fileType == "yaml"){
-                    input = ""
-                }
-
-                if(input == null){
-                    throw new Exception("[Samplesheet Error] Line ${rowCount} does not contain an input for field '${key}'.")
-                }
-                else if(input == "" && key in requiredFields){
-                    throw new Exception("[Samplesheet Error] Line ${rowCount} contains an empty input for required field '${key}'.")
+                if((input == null || input == "") && key in requiredFields){
+                    throw new Exception("[Samplesheet Error] Line ${rowCount} does not contain an input for required field '${key}'.")
                 }
                 else if(field.value.unique){
                     if(!(key in uniques)){uniques[key] = []}
-                    if(input in uniques[key]){
+                    if(input in uniques[key] && input){
                         throw new Exception("[Samplesheet Error] The '${key}' value needs to be unique. '${input}' was found twice in the samplesheet.")
                     }
                     uniques[key].add(input)
                 }
-                else if(!(input ==~ regexPattern) && input != '') {
+                else if(!(input ==~ regexPattern) && input != '' && input) {
                     throw new Exception("[Samplesheet Error] The '${key}' value on line ${rowCount} does not match the pattern '${regexPattern}'.")
                 }
-                
+
                 if(metaNames) {
                     for(name : metaNames.tokenize(',')) {
-                        meta[name] = input != '' ? input.replace(' ', '_') : field.value.default ?: null
+                        meta[name] = (input != '' && input) ? input.replace(' ', '_') : field.value.default ?: null
                     }
                 }
                 else {
-                    def inputFile = input != '' ? Nextflow.file(input) : field.value.default ? Nextflow.file(field.value.default) : []
+                    def inputFile = (input != '' && input) ? Nextflow.file(input) : field.value.default ? Nextflow.file(field.value.default) : []
                     if( inputFile != [] && !inputFile.exists() ){
                         throw new Exception("[Samplesheet Error] The '${key}' file (${input}) on line ${rowCount} does not exist.")
                     }
